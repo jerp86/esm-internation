92858b568863e6a12a6e29fe2bcfc9e2aef81d2d
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2022-10-20 23:15:17 -0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,36 +1,26 @@
-import DraftLog from "draftlog";
-import chalk from "chalk";
-import chalkTable from "chalk-table";
-import readLine from "readline";
-
 import database from "./../database.json";
-import Person from "./person.js";
-
-DraftLog(console).addLineListener(process.stdin);
+import TerminalController from "./terminalController.js";
 
 const DEFAULT_LANG = "pt-BR";
-const options = {
-  leftPad: 2,
-  columns: [
-    { field: "id", name: chalk.cyan("ID") },
-    { field: "vehicles", name: chalk.magenta("Vehicles") },
-    { field: "kmTraveled", name: chalk.blue("KM Traveled") },
-    { field: "from", name: chalk.green("From") },
-    { field: "to", name: chalk.yellow("To") },
-  ],
-};
+const STOP_TERM = ":q";
 
-const table = chalkTable(
-  options,
-  database.map((item) => new Person(item).formatted(DEFAULT_LANG))
-);
-const print = console.draft(table);
+const terminalController = new TerminalController();
+terminalController.initializeTerminal(database, DEFAULT_LANG);
 
-const terminal = readLine.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+async function mainLoop() {
+  try {
+    const answer = await terminalController.question();
+    if (answer === STOP_TERM) {
+      terminalController.closeTerminal();
+      console.log("process finished!");
+      return;
+    }
 
-terminal.question("Qual é o seu nome?", (msg) => {
-  console.log("msg", msg.toString());
-});
+    return mainLoop();
+  } catch (error) {
+    console.error("DEU RUIM**", error);
+    return mainLoop();
+  }
+}
+
+await mainLoop();
