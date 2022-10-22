b2ddd4769c36fd7f7b5112d5908a5ad7939f233a
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2022-10-22 12:03:09 -0300

    add examples another languages and save method

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import database from "./../database.json";
 import Person from "./person.js";
+import { save } from "./repository.js";
 import TerminalController from "./terminalController.js";
 
 const DEFAULT_LANG = "pt-BR";
+// const DEFAULT_LANG = "es"; // espanhol
+// const DEFAULT_LANG = "en"; // inglês
+// const DEFAULT_LANG = "rus"; // russo
+// const DEFAULT_LANG = "zh-Hans-CN"; // chinês simplificado
+// const DEFAULT_LANG = "sr-Cyrl"; // Sérvio escrito usando o script cirílico
 const STOP_TERM = ":q";
 
 const terminalController = new TerminalController();
@@ -19,6 +25,7 @@ async function mainLoop() {
 
     const person = Person.generateInstanceFromString(answer);
     terminalController.updateTable(person.formatted(DEFAULT_LANG));
+    await save(person);
 
     return mainLoop();
   } catch (error) {
